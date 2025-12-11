704b3c80cdf426ebce4a7f22aa92f54258e52d58
Lahari <mslahari05@gmail.com>
2025-12-11 11:29:09 +0530

    Comment out the add-users route in Stdrouter.js

--- Routers/Stdrouter.js
+++ Routers/Stdrouter.js
@@ -2,5 +2,5 @@ import express from 'express';
 import {getStudents,  addData } from "../Controllers/Stdcontrollers.js";
 const router=express.Router();
 router.get('/get-users',getStudents);
-router.post('/add-users',addData)
+// router.post('/add-users',addData);
 export default router;
